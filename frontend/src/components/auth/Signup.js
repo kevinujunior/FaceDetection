7e20fa0ef8c2f3221f4f89f87b9d6e32c7980514
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-07-24 18:33:38 +0530

    Re-designed home and navbar

--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Navbar from "../Navbar";
 
 const Register = () => {
-  return (
-    <div>
-      Register Page
-    </div>
-  );
+  return <div>Register Page</div>;
 };
 
 export default Register;
