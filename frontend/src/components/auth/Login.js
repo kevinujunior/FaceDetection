7202b71be8257a3305585d96ee604b576dbd6b71
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-07-26 18:54:36 +0530

    Integrated login and signup from backend

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,9 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { postExistingAuth } from "../../api";
 
 const Login = () => {
-  const handleSubmit = () => {};
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username && password) {
+      postExistingAuth({
+        username: username,
+        password: password,
+      });
+    }
+  };
   var history = useHistory();
 
   useEffect(() => {
@@ -13,8 +25,18 @@ const Login = () => {
     <div className="login">
       <h2>Login Account</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
       </form>
       <span className="or">or connect with</span>
