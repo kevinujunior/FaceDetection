df28eff341688437200fc283059c2a15aed99478
Navneet-Singh-123 <snavneet561@gmail.com>
2021-07-30 00:21:31 +0530

    google facebook login tests

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Redirect, useHistory } from "react-router-dom";
+import { Redirect, useHistory, Link } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { postExistingAuth } from "../../api";
+import GitHubLogin from "react-github-login";
+import GoogleLogin from "react-google-login";
+import FacebookLogin from "react-facebook-login";
 
 const Login = ({ history }) => {
   const [username, setUsername] = useState("");
@@ -20,6 +23,14 @@ const Login = ({ history }) => {
     }
   };
   var history = useHistory();
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+
+  const fbResponse = (response) => {
+    console.log(response);
+  };
 
   useEffect(() => {
     history.replace("/login");
@@ -43,6 +54,18 @@ const Login = ({ history }) => {
         <button type="submit">Login</button>
       </form>
       <span className="or">or connect with</span>
+      <GoogleLogin
+        clientId="579043395450-e68o76r9cag3d8vcg625qld2uiai4o06.apps.googleusercontent.com"
+        buttonText="LOGIN WITH GOOGLE"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+      <FacebookLogin
+        textButton="LOGIN WITH FACEBOOK"
+        appId="992007751609658"
+        fields="name,email,picture"
+        callback={fbResponse}
+      />
       <div className="social">
         <i className="bi bi-facebook"></i>
         <i className="bi bi-google"></i>
