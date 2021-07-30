75204ecd058f2bd0851177e52cd14c70f7d3c72a
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-07-30 15:41:07 +0530

    Implemented responsive design

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -37,41 +37,45 @@ const Login = ({ history }) => {
     history.replace("/login");
   }, [history]);
   return (
-    <div className="login">
-      <h2>Login Account</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
-      <span className="or">or connect with</span>
-      <GoogleLogin
-        clientId="579043395450-e68o76r9cag3d8vcg625qld2uiai4o06.apps.googleusercontent.com"
-        buttonText="LOGIN WITH GOOGLE"
-        onSuccess={googleResponse}
-        onFailure={googleResponse}
-      />
-      <FacebookLogin
-        textButton="LOGIN WITH FACEBOOK"
-        appId="992007751609658"
-        fields="name,email,picture"
-        callback={fbResponse}
-      />
-      <div className="social">
-        <i className="bi bi-facebook"></i>
-        <i className="bi bi-google"></i>
-        <i className="bi bi-linkedin"></i>
-        <i className="bi bi-github"></i>
+    <div className="login_container">
+      <div className="login">
+        <h2>Login Account</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+        <span className="or">or connect with</span>
+        <div className="auth">
+          <GoogleLogin
+            clientId="579043395450-e68o76r9cag3d8vcg625qld2uiai4o06.apps.googleusercontent.com"
+            buttonText="LOGIN WITH GOOGLE"
+            onSuccess={googleResponse}
+            onFailure={googleResponse}
+          />
+          <FacebookLogin
+            textButton="LOGIN WITH FACEBOOK"
+            appId="992007751609658"
+            fields="name,email,picture"
+            callback={fbResponse}
+          />
+        </div>
+        {/* <div className="social">
+          <i className="bi bi-facebook"></i>
+          <i className="bi bi-google"></i>
+          <i className="bi bi-linkedin"></i>
+          <i className="bi bi-github"></i>
+        </div> */}
       </div>
     </div>
   );
--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -26,37 +26,39 @@ const Register = () => {
     history.replace("/signup");
   }, [history]);
   return (
-    <div className="login">
-      <h2>Sign-up Account</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <button type="submit">Sign-up</button>
-      </form>
-      <span className="or">
-        <span>or connect with</span>
-      </span>
-      <div className="social">
-        <i className="bi bi-facebook"></i>
-        <i className="bi bi-google"></i>
-        <i className="bi bi-linkedin"></i>
-        <i className="bi bi-github"></i>
+    <div className="login_container">
+      <div className="login">
+        <h2>Sign-up Account</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button type="submit">Sign-up</button>
+        </form>
+        <span className="or">
+          <span>or connect with</span>
+        </span>
+        <div className="social">
+          <i className="bi bi-facebook"></i>
+          <i className="bi bi-google"></i>
+          <i className="bi bi-linkedin"></i>
+          <i className="bi bi-github"></i>
+        </div>
       </div>
     </div>
   );
